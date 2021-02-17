7334509058299993953a6720710be35b4d0e10ae
Andrew Yourtchenko <ayourtch@gmail.com>
2021-02-17 21:41:50 +0000

    add more debugs

--- misc/debug.js
+++ misc/debug.js
@@ -1,7 +1,15 @@
 console.log("Hello from phantomjs");
+var fs = require('fs');
+var page = require('webpage').create();
+console.log("fs and page created");
+
 
 setTimeout(function() {
              console.log("Timeout on phantomjs, exiting");
              phantom.exit();
        }, 1000);
+
+page.open("http://stdio.be/", function(st) {
+  console.log("Status: " + st);
+});
 
