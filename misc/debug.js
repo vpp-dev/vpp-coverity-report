7089026b1b3856564d820d116a2b9c688d80d562
Andrew Yourtchenko <ayourtch@gmail.com>
2021-02-17 22:00:20 +0000

    carefully add second stage

--- misc/debug.js
+++ misc/debug.js
@@ -24,6 +24,15 @@ setTimeout(function() {
              console.log("Status: " + st);
              if (st === "success") {
                do_login(page);
+	       setTimeout(function() {
+		       new_url = "https://scan.coverity.com/projects/fd-io-vpp/view_defects";
+                       page.open(new_url, function(status) {
+                            console.log("Second stage status: " + status);
+                            if(status === "success") {
+                                 page.render('example.png');
+                            }
+                       });
+	       }, 15000);
 	     }
           });
        }, 500);
