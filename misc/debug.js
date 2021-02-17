e9b10f8cfe45cd809bdb7a8f24bd75d868c677ab
Andrew Yourtchenko <ayourtch@gmail.com>
2021-02-17 21:48:13 +0000

    increase the timeout on debug

--- misc/debug.js
+++ misc/debug.js
@@ -7,7 +7,7 @@ console.log("fs and page created");
 setTimeout(function() {
              console.log("Timeout on phantomjs, exiting");
              phantom.exit();
-       }, 1000);
+       }, 10000);
 
 page.open("https://scan.coverity.com/users/sign_in", function(st) {
   console.log("Status: " + st);
